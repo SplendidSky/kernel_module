3ae4480b1bf4a61096e7716714376ecb46a3285d
田各 <splendidsky.cwc@alibaba-inc.com>
2019-05-29 20:33:47 +0800

    not taint kernel

--- vser1.c
+++ vser1.c
@@ -2,11 +2,19 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-int int_module(void) {
-	printk("module init\n");
+static int __init vser_init(void) {
+	printk("vser_init\n");
 	return 0;
 }
 
-void cleanup_module(void) {
-	printk("module cleanup\n");
+static void __exit vser_exit(void) {
+	printk("vser_exit\n");
 }
+
+module_init(vser_init);
+module_exit(vser_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Weichen Chen <splendidsky1117@gmail.com>");
+MODULE_DESCRIPTION("A simple module");
+MODULE_ALIAS("virtual-serial");
