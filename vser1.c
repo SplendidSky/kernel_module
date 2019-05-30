5488404ee8a3152ba09d76df0e1c0d0ec665a46e
田各 <splendidsky.cwc@alibaba-inc.com>
2019-05-30 15:30:07 +0800

    param kernel module

--- vser1.c
+++ vser1.c
@@ -4,8 +4,23 @@
 
 extern void bar(void);
 
+static int baudrate = 9600;
+static int port[4] = { 0, 1, 2, 3 };
+static char *name = "vser";
+
+moudule_param(baudrate, int, S_IRUGO);
+moudule_param_array(port, int, NULL, S_IRUGO);
+moudule_param(name, charp, S_IRUGO)
+
+
 static int __init vser_init(void) {
 	printk("vser_init\n");
+	printk("baudrate: %d\n", baudrate);
+	printk("port: ");
+	for (int i = 0; i < ARRAY_SIZE(port); i++) {
+		printk("%d ", port[i]);
+	}
+	printk("name: %s\n", name);
 	bar();
 	return 0;
 }
